396a3feee5b7142b2eaf4e0730540c9af3b702bb
Diego-sci01 <diegosantosantana2008@gmail.com>
2025-08-05 22:43:52 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Bobby'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getRandomNumber(a, b) {
+  return Math.floor(Math.random() * (b - a + 1) + a);
+}
+
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
+
+window.onload = function () {
   console.log("Hello Rigo from the console!");
+  document.body.innerHTML =
+    getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when);
 };
